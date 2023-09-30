4d7982dedaebacd6c2af8709b01afe46128849e1
EuricoNobre <132778705+EuricoNobre@users.noreply.github.com>
2023-09-30 21:27:46 +0000

    Incluindo paragrafo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <><h1>Acíria, você é MUUUIIITO Fofinha!!</h1><p>Assinado Firifenta</p></>
+    return <><h1>Acíria, você é MUUUIIITO Fofinha!!</h1><p>Assinado Firifenta.</p></>
 }
 
 export default Home;
